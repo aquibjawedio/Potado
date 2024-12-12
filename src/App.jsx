1f46a4b1497808ebc17b9140123f67589e77dbfc
Aquib Jawed <info.aquibjawed@gmail.com>
2024-12-12 22:15:24 +0530

    Creation

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <button className="bg-blue-600 py-2 px-5 rounded-md hover:bg-blue-500">Click Here</button>
+    </>
+  )
+}
+
+export default App
